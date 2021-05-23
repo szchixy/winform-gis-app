a40db5b0ce611b6c5d640045739d4271f12e0731
szchixy <szchixy@outlook.com>
2021-05-23 13:16:03 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ListStatus = new System.Windows.Forms.ComboBox();
+            this.LabelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(0, 35);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(994, 627);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // ListStatus
+            // 
+            this.ListStatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ListStatus.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ListStatus.FormattingEnabled = true;
+            this.ListStatus.Items.AddRange(new object[] {
+            "无",
+            "手绘",
+            "点",
+            "线",
+            "面"});
+            this.ListStatus.Location = new System.Drawing.Point(60, 5);
+            this.ListStatus.Margin = new System.Windows.Forms.Padding(0);
+            this.ListStatus.Name = "ListStatus";
+            this.ListStatus.Size = new System.Drawing.Size(81, 25);
+            this.ListStatus.TabIndex = 2;
+            this.ListStatus.Tag = "无";
+            this.ListStatus.SelectedIndexChanged += new System.EventHandler(this.ListStatus_SelectedIndexChanged);
+            // 
+            // LabelStatus
+            // 
+            this.LabelStatus.AutoSize = true;
+            this.LabelStatus.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.LabelStatus.Location = new System.Drawing.Point(5, 5);
+            this.LabelStatus.Name = "LabelStatus";
+            this.LabelStatus.Size = new System.Drawing.Size(46, 21);
+            this.LabelStatus.TabIndex = 3;
+            this.LabelStatus.Text = "工具:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 661);
+            this.Controls.Add(this.LabelStatus);
+            this.Controls.Add(this.ListStatus);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox ListStatus;
+        private System.Windows.Forms.Label LabelStatus;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;
+        private Pen pen;
+        private Brush brush;
+        private int x = -1;
+        private int y = -1;
+        private enum process {Nothing, FreePen, MultiPoint, MultiLineString, MultiPolygon};
+        private process status;
+        private bool drawing = false;
+        List<Point> polygon = null;
+        private int polygonCount = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            pen = new Pen(Color.Red, 2);
+            brush = new SolidBrush(Color.Blue);
+            pen.StartCap = pen.EndCap = LineCap.Round;
+            status = process.Nothing;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(drawing)
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    switch (status)
+                    {
+                        case process.MultiLineString:
+                            g.DrawLine(pen, new Point(x, y), e.Location);
+                            x = e.X;
+                            y = e.Y;
+                            break;
+                        case process.MultiPolygon:
+                            polygon.Add(e.Location);
+                            if (++polygonCount >= 3)
+                                g.FillPolygon(brush, polygon.ToArray());
+                            break;
+                        default: break;
+                    }
+                }
+                else if(e.Button == MouseButtons.Right)
+                {
+                    switch (status)
+                    {
+                        case process.MultiLineString:
+                            drawing = false;
+                            break;
+                        case process.MultiPolygon:
+                            polygonCount = 0;
+                            polygon = null;
+                            drawing = false;
+                            break;
+                        default: break;
+                    }
+                }
+            }
+            else if(!drawing && e.Button == MouseButtons.Left)
+            {
+                drawing = true;
+                switch (status)
+                {
+                    case process.MultiPoint:
+                        g.DrawEllipse(pen, new Rectangle(e.X - 1, e.Y - 1, 2, 2));
+                        break;
+                    case process.FreePen:
+                    case process.MultiLineString:
+                        x = e.X;
+                        y = e.Y;
+                        break;
+                    case process.MultiPolygon:
+                        polygon = new List<Point>{e.Location};
+                        polygonCount = 1;
+                        break;
+                    default: break;
+                }
+            }
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drawing && e.Button == MouseButtons.Left)
+            {
+                switch (status)
+                {
+                    case process.FreePen:
+                        g.DrawLine(pen, new Point(x, y), e.Location);
+                        x = e.X;
+                        y = e.Y;
+                        break;
+                    default: break;
+                }
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (drawing)
+            {
+                switch (status)
+                {
+                    case process.MultiLineString:
+                    case process.MultiPolygon:
+                        break;
+                    default:
+                        drawing = false;
+                        break;
+                }
+            }
+        }
+
+        private void ListStatus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(ListStatus.SelectedIndex)
+            {
+                case 0: status = process.Nothing; break;
+                case 1: status = process.FreePen; break;
+                case 2: status = process.MultiPoint; break;
+                case 3: status = process.MultiLineString; break;
+                case 4: status = process.MultiPolygon; break;
+                default: break;
+            }
+        }
+    }
+}
